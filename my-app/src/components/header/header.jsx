19d6ad226fdcf4395d88f00854ac0de22acd3bc8
EdilAskarbekov <edilaskarbekov01@gmail.com>
2024-05-15 23:58:15 +0600

    fix
    Signed-off-by: EdilAskarbekov <edilaskarbekov01@gmail.com>

---
+++ my-app/src/components/header/header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <>
+        <header>
+            <div className={headerWrap}>
+                <div className={logo}>
+                    <img src={}/>
+                </div>
+                <ul className="navMenu">
+                    <li></li>
+                    <li></li>
+
+                </ul>
+            </div>
+        </header>
+        </>
+    )
+}
